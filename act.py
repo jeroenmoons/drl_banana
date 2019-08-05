2e9198c02bd2f9665a08f48f0846c910bf01b5d0
Jeroen Moons <jeroen.moons@intracto.com>
2019-08-05 17:40:12 +0200

    Cleaner episode run loop

--- act.py
+++ act.py
@@ -2,18 +2,16 @@ def run_episode(env, agent):
     """
     Shows a UnityAgent perform a single episode in the specified Unity ML environment.
     """
+    done = False
     score = 0  # initialize score
 
     env_info = env.reset(train_mode=False)[agent.brain_name]  # reset the environment
 
-    while True:
+    while not done:
         state = env_info.vector_observations[0]
-        action = agent.select_action(state)  # pick random action
+        action = agent.select_action(state)  # choose an action
         env_info = env.step(action)[agent.brain_name]  # execute that action
         done = env_info.local_done[0]  # check if episode has finished
         score += env_info.rewards[0]  # update score with the reward
-
-        if done:  # stop if episode has finished
-            break
 
     return score
