a1689d6f1eddb989abd885647b7c2ea77ce4a9b8
Jeroen Moons <jeroen.moons@intracto.com>
2019-08-06 15:04:05 +0200

    added a replay buffer to the dqn agent

--- agent/dqn.py
+++ agent/dqn.py
@@ -1,7 +1,9 @@
 import numpy as np
 import torch
+
 from agent.base import UnityAgent
 from agent.estimate.neural import FullyConnectedNetwork
+from collections import namedtuple, deque
 
 
 class DqnAgent(UnityAgent):
@@ -10,11 +12,15 @@ class DqnAgent(UnityAgent):
     # Default params
     DEVICE_DEFAULT = 'cpu'  # pytorch device
 
+    HIDDEN_LAYER_SIZES_DEFAULT = (50, 50)  # default q network hidden layer sizes
+
+    REPLAY_BUFFER_SIZE = 100000  # max nr of experiences in memory
+
     EPSILON_DEFAULT = 1.  # starting value for epsilon
     EPSILON_DECAY_DEFAULT = .9999  # used to decay epsilon over time
     EPSILON_MIN_DEFAULT = .005  # minimum value for decayed epsilon
 
-    HIDDEN_LAYER_SIZES_DEFAULT = (50, 50)  # default q network hidden layer sizes
+    LEARN_BATCH_SIZE = 50  # batch size to use when learning from memory
 
     def __init__(self, brain_name, state_size, action_size, params):
         super().__init__(brain_name, state_size, action_size, params)
@@ -26,6 +32,12 @@ class DqnAgent(UnityAgent):
         self.epsilon = params.get('epsilon', self.EPSILON_DEFAULT)
         self.epsilon_decay = params.get('epsilon_decay', self.EPSILON_DECAY_DEFAULT)
         self.epsilon_min = params.get('epsilon_min', self.EPSILON_MIN_DEFAULT)
+
+        self.learn_batch_size = params.get('learn_batch_size', self.LEARN_BATCH_SIZE)
+
+        # memory
+        self.memory_size = params.get('memory_size', self.REPLAY_BUFFER_SIZE)
+        self.memory = ReplayBuffer(action_size, self.memory_size)
 
         # online and target Q-network models
         self.hidden_layer_sizes = params.get('hidden_layer_sizes', self.HIDDEN_LAYER_SIZES_DEFAULT)
@@ -66,9 +78,29 @@ class DqnAgent(UnityAgent):
     def get_params(self):
         return {
             'device': self.device,
+            'memory_size': self.memory_size,
+            'learn_batch_size': self.learn_batch_size,
             'epsilon': self.epsilon,
             'epsilon_decay': self.epsilon_decay,
             'epsilon_min': self.epsilon_min,
             'online_network': self.online_network,
             'target_network': self.target_network
         }
+
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, action_size, buffer_size):
+        self.action_size = action_size
+        self.buffer = deque(maxlen=buffer_size)
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.buffer.append(e)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.buffer)
