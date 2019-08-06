68cf733287dd2e4e69bd97de3877a0548efe0c1a
Jeroen Moons <jeroen.moons@intracto.com>
2019-08-06 13:02:11 +0200

    agent params strategy

--- agent/dqn.py
+++ agent/dqn.py
@@ -3,9 +3,21 @@ from agent.base import UnityAgent
 
 
 class DqnAgent(UnityAgent):
+    # Default params
+    EPSILON_DEFAULT = 1.
+    EPSILON_DECAY_DEFAULT = .9999
+    EPSILON_MIN_DEFAULT = .005
+
     """
     Chooses actions using a NN to estimate action values.
     """
+    def __init__(self, brain_name, state_size, action_size, params):
+        super().__init__(brain_name, state_size, action_size)
+
+        self.epsilon = params.get('epsilon', self.EPSILON_DEFAULT)
+        self.epsilon_decay = params.get('epsilon', self.EPSILON_DECAY_DEFAULT)
+        self.epsilon_min = params.get('epsilon', self.EPSILON_MIN_DEFAULT)
+
     def select_action(self, state):
         return np.random.choice(self.action_size)  # TODO: implement DQN epsilon-greedy action selection
 
